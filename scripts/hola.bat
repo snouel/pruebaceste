35e7ecbb3e7a0cb3c510e6f99ec453c23355be11
snouel <104464911+snouel@users.noreply.github.com>
2025-11-25 02:34:10 +0000

    correccion hola.bat

--- scripts/hola.bat
+++ scripts/hola.bat
@@ -1,3 +1,3 @@
 @echo off
 echo Hola desde el repo en %DATE% %TIME%
-echo Resultado del build #%BUILD_NUMBER% > buil-%BUILD_NUMBER%.txt
+echo Resultado del build #%BUILD_NUMBER% > build-%BUILD_NUMBER%.txt
